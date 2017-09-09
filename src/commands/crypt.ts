76d7e5749152c019f4ddcd52a2fa9c114771061e
Brandon Tosch <BrandonTosch@gmail.com>
2017-09-09 06:04:10 -0700

    implemented output preparation for block formatting

--- src/commands/crypt.ts
+++ src/commands/crypt.ts
@@ -11,6 +11,7 @@ export namespace commands {
 
     export interface CryptCommand {
         execute(input: string): Promise<string>;
+        prepareOutput(output: string): string;
     }
 
     export function getInstance(cryptKind: CryptKind, rawConfig: WorkspaceConfiguration, execCommand: exec.commands.ExecCommand): CryptCommand {
@@ -38,9 +39,14 @@ export namespace commands {
             return input;
         }
 
+        public prepareOutput(output: string): string {
+            return output.trim();
+        }
+
         public execute(input: string): Promise<string> {
             const execString = this.buildExecString();
-            return this.execCommand.execute(execString, input)
+            const preparedInput = this.prepareInput(input);
+            return this.execCommand.execute(execString, preparedInput);
         }
     }
 
@@ -62,13 +68,13 @@ export namespace commands {
 
         removeAll(source: string, removes: string[]): string {
             var result = source;
-            
+
             removes.forEach((remove) => {
-                while(result.indexOf(remove) != -1) {
+                while (result.indexOf(remove) != -1) {
                     result = result.replace(remove, "");
                 }
             });
-        
+
             return result;
         }
     }
@@ -83,6 +89,21 @@ export namespace commands {
 
         protected buildExecString(): string {
             return `${this.currentConfig.eyamlPath} encrypt -o ${this.currentConfig.outputFormat} --stdin --pkcs7-public-key "${this.currentConfig.publicKeyPath}"`;
+        }
+
+        public prepareOutput(output: string): string {
+            switch (this.currentConfig.outputFormat) {
+                case config.OutputFormat.Block:
+                    let preparedOutput = "";
+
+                    for (let i = 0; i < output.length; i += this.currentConfig.outputBlockSize) {
+                        preparedOutput += output.substring(i, i + this.currentConfig.outputBlockSize) + "\n";
+                    }
+
+                    return super.prepareOutput(preparedOutput);
+                default:
+                    return super.prepareOutput(output);
+            }
         }
     }
 }
